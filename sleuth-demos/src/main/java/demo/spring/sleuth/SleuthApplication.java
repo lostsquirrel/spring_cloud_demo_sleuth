82e20737595845876350426c870b510948b13348
lostsquirrel <songli1988@gmail.com>
2017-12-24 21:46:52 +0800

    add simple rabbit client

--- sleuth-demos/src/main/java/demo/spring/sleuth/SleuthApplication.java
+++ sleuth-demos/src/main/java/demo/spring/sleuth/SleuthApplication.java
@@ -2,11 +2,18 @@ package demo.spring.sleuth;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SleuthApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SleuthApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
     }
 }
