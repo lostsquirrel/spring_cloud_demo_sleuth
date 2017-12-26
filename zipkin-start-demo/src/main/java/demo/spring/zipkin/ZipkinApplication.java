fe18579cd9025c761c692f8653539ad62ce334ce
lostsquirrel <songli1988@gmail.com>
2017-12-26 13:36:55 +0800

    add eureka client dependency

--- zipkin-start-demo/src/main/java/demo/spring/zipkin/ZipkinApplication.java
+++ zipkin-start-demo/src/main/java/demo/spring/zipkin/ZipkinApplication.java
@@ -2,7 +2,9 @@ package demo.spring.zipkin;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringCloudApplication
 @EnableZipkinStreamServer
@@ -10,4 +12,8 @@ public class ZipkinApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZipkinApplication.class).web(true).run(args);
     }
+
+    @Configuration
+    @EnableDiscoveryClient
+    static class CloudConfig {}
 }
